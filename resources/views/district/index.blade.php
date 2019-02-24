a4a9988ed3eafdd478cfb1874c4e5ab8494fcb6c
Chavalit Koweerawong <chavalit.kow@gmail.com>
2019-02-24 23:46:11 +0700

    EE

--- resources/views/district/index.blade.php
+++ resources/views/district/index.blade.php
@@ -8,29 +8,116 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Create address</title>
 
 
   </head>
   <body>
-    <h1>Hello, world!</h1>
-    <div>
-      <select id="input_province" onchange="showAmphoes()">
-        <option value="">กรุณาเลือกจังหวัด</option>
-      </select>
-    </div>
-    <div>
-      <select id="input_amphoe" onchange="showDistricts()">
-        <option value="">กรุณาเลือกอำเภอ</option>
-      </select>
-    </div>
-    <div>
-      <select id="input_district" onchange="showZipcode()">
-        <option value="">กรุณาเลือกตำบล</option>
-      </select>
-    </div>
-    <div>
-      <input id="input_zipcode" placeholder="รหัสไปรษณีย์" />
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Dropdown
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <div class="dropdown-divider"></div>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </div>
+      </div>
+    </nav>
+    <div class="container">
+        <h1 class="mt-5">ที่อยู่สำหรับจัดส่งสินค้า</h1>
+        <div class="row">
+            <div class="col-sm-8">
+                <div class="card mt-5"  >
+                  <div class="card-body">
+                    <form >
+                        <div class="form-group row">
+                          <label for="staticEmail" class="col-sm-2 col-form-label">ชื่อ-นามสกุล</label>
+                          <div class="col-sm-10">
+                            <input class="form-control" id="fullname" placeholder="โปรดระบุ ..." />
+                          </div>
+                        </div>
+                        <div class="form-group row">
+                          <label for="staticEmail" class="col-sm-2 col-form-label">ที่อยู่</label>
+                          <div class="col-sm-10">
+                            <input class="form-control" id="address" placeholder="โปรดระบุ ..." />
+                          </div>
+                        </div>
+                      <div class="form-group row">
+                        <label for="staticEmail" class="col-sm-2 col-form-label">จังหวัด</label>
+                        <div class="col-sm-10">
+                          <select class="form-control" id="input_province" onchange="showAmphoes()">
+                            <option value="">กรุณาเลือกจังหวัด</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">อำเภอ</label>
+                        <div class="col-sm-10">
+                            <select class="form-control" id="input_amphoe" onchange="showDistricts()">
+                              <option value="">กรุณาเลือกอำเภอ</option>
+                            </select>
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">ตำบล</label>
+                        <div class="col-sm-10">
+                            <select class="form-control" id="input_district" onchange="showZipcode()">
+                              <option value="">กรุณาเลือกตำบล</option>
+                            </select>
+                        </div>
+                      </div>
+                      <div class="form-group row">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">รหัสไปรษณีย์</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" id="input_zipcode" placeholder="รหัสไปรษณีย์" />
+                        </div>
+                      </div>
+                      <div class="form-group row">
+
+                        <div class="col-sm-12 text-center">
+                            <button type="submit" class="btn btn-primary" >Submit</button>
+                        </div>
+                      </div>
+
+                    </form>
+
+                  </div>
+                </div>
+
+
+            </div>
+        </div>
+
+
+
     </div>
 
     <!-- Optional JavaScript -->
