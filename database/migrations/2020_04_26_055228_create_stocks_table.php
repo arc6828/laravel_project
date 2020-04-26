8bd6ed6f6b58d27ba44155067f2fe882a9bb052a
DoubleT-Meesa <51850572+DoubleT-Meesa@users.noreply.github.com>
2020-04-26 13:00:28 +0700

    Create 2020_04_26_055228_create_stocks_table.php

---
+++ database/migrations/2020_04_26_055228_create_stocks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->integer('price')->nullable();
+            $table->float('discount')->nullable();
+            $table->string('product')->nullable();
+            $table->date('date')->nullable();
+            $table->text('detail')->nullable();
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
